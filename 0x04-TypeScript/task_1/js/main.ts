99ab08f2f9df2c7f8bf6f0914506357b2e889363
awinooliyo <erick.otieno89@gmail.com>
2024-07-05 11:35:59 +0300

    task 4 solution

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -45,7 +45,41 @@ const printTeacher: printTeacherFunction = (firstName, lastName) => {
   return `${firstName.charAt(0)}. ${lastName}`;
 };
 
+interface studentClassConstructor {
+  new(firstName: string, lastName: string): studentClassInterface;
+};
+
+interface studentClassInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+};
+
+class StudentClass implements studentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+	this.lastName = lastName;
+  };
+
+  workOnHomework(): string {
+    return "Currently working";
+  };
+
+  displayName(): string {
+    return this.firstName;
+  };
+};
+
+const student = new StudentClass("Amerix", "Lechuli");
+
 console.log(firstTeacher);
 console.log(secondTeacher);
 console.log(firstDirector);
 console.log(printTeacher("Elimuli", "Ekenya"));
+
+console.log(student.displayName());
+console.log(student.workOnHomework());
