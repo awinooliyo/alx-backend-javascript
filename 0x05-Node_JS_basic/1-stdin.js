5091305858f08379d3890ac5092351d605205c92
awinooliyo <erick.otieno89@gmail.com>
2024-10-01 18:43:18 +0300

    task 2

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,6 +3,12 @@
  * and displays a message containing the username.
  *
  * When the user ends the program, a closing message is displayed.
+ *@example 
+ Output:
+ *// Welcome to Holberton School, what is your name?
+ *// Erick
+ *// Your name is: Erick
+ *// This importabt Software is now closing
  */
 process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
