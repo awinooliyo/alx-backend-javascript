bb9c75fe5e1e78494f2f82b3504bd2fc2fb16d79
awinooliyo <erick.otieno89@gmail.com>
2024-10-01 19:17:02 +0300

    task 2

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,20 +3,21 @@
  * and displays a message containing the username.
  *
  * When the user ends the program, a closing message is displayed.
- *@example 
- Output:
- *// Welcome to Holberton School, what is your name?
- *// Erick
- *// Your name is: Erick
- *// This importabt Software is now closing
+ * @example
+ * Output:
+ * // Welcome to Holberton School, what is your name?
+ * // Erick
+ * // Your name is: Erick
+ * // This important software is now closing
  */
+
 process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
 process.stdin.on("readable", () => {
   const userName = process.stdin.read();
 
   if (userName) {
-    process.stdout.write(`Your name is: ${userName}`);
+    process.stdout.write(`Your name is: ${userName.toString().trim()}\n`);
   }
 });
 
