dd5e2b085c772444a2ad3c5bab547e9dab74e4b1
awinooliyo <erick.otieno89@gmail.com>
2024-10-02 04:22:16 +0300

    task 1 corrected

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -11,16 +11,16 @@
  * // Your name is: Erick
  * // This important software is now closing
  */
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on("readable", () => {
+process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
 
-  if (chunk) {
+  if (chunk !== null) {
     process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
-process.stdin.on("end", () => {
-  process.stdout.write("This important software is now closing\n");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
