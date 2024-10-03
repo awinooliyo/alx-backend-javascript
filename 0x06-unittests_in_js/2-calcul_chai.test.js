55585a36fefff50c63e8cb8b75ea2b873c65e30e
awinooliyo <erick.otieno89@gmail.com>
2024-10-03 19:03:26 +0300

    testing with chai

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,35 +1,46 @@
 /* eslint-disable jest/expect-expect */
 /* eslint-disable jest/prefer-expect-assertions */
-import { expect } from 'chai';
-import { calculateNumber } from './2-calcul_chai.js';
-
-// You can assume a and b are always number
+const { expect } = require('chai');
+const calculateNumber = require('./2-calcul_chai');
 
 describe('calculateNumber', () => {
-  it('handles adding two rounded numbers when type is SUM', () => {
-    expect(calculateNumber('SUM', 1.2, 3.7)).to.equal(5);
-    expect(calculateNumber('SUM', -1.2, -3.7)).to.equal(-5);
+  describe('SUM', () => {
+    it('should return the sum of two rounded numbers', () => {
+      expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+    });
+
+    it('should handle negative numbers', () => {
+      expect(calculateNumber('SUM', -1.4, 4.5)).to.equal(4);
+    });
   });
 
-  it('handles subtracting two rounded numbers when type is SUBTRACT', () => {
-    expect(calculateNumber('SUBTRACT', 5.9, 2.3)).to.equal(4);
-    expect(calculateNumber('SUBTRACT', -5.9, -2.3)).to.equal(-4);
+  describe('SUBTRACT', () => {
+    it('should return the difference between two rounded numbers', () => {
+      expect(calculateNumber('SUBTRACT', 5.9, 2.3)).to.equal(4);
+    });
+
+    it('should handle negative numbers', () => {
+      expect(calculateNumber('SUBTRACT', -5.9, -2.3)).to.equal(-4);
+    });
   });
 
-  it('handles dividing two rounded numbers when type is DIVIDE', () => {
-    expect(calculateNumber('DIVIDE', 8, 2)).to.equal(4);
+  describe('DIVIDE', () => {
+    it('should return the division of two rounded numbers', () => {
+      expect(calculateNumber('DIVIDE', 8.9, 2.2)).to.equal(4);
+    });
+
+    it('should return "Error" when dividing by 0', () => {
+      expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+    });
+
+    it('should handle division with negative numbers', () => {
+      expect(calculateNumber('DIVIDE', -1.4, 4.5)).to.equal(-0.2);
+    });
   });
 
-  it('handles division by a negative', () => {
-    expect(calculateNumber('DIVIDE', -1.4, 4.5)).to.equal(-0.2); 
-  });
-
-  it('returns Error when dividing by 0', () => {
-    expect(calculateNumber('DIVIDE', 4, 0)).to.equal('Error');
-    expect(calculateNumber('DIVIDE', -8, 0)).to.equal('Error');
-  });
-
-  it('throws an error when an invalid type is provided', () => {
-    expect(() => calculateNumber('INVALID_TYPE', 1, 2)).to.throw(Error);
+  describe('Invalid Type', () => {
+    it('should throw an error for invalid type', () => {
+      expect(() => calculateNumber('MULTIPLY', 1, 2)).to.throw('Invalid type');
+    });
   });
 });
